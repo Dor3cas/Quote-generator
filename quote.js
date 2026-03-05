5409f7066a8c91641341405c1bdf18728ac58573
Dor3cas <niyigenadorcas6@gmail.com>
2026-03-05 10:07:32 +0200

    connected to mongodb and added env

--- quote.js
+++ quote.js
@@ -1,37 +1,43 @@
- const express = require('express');
- const  mongoose = require('mongoose');
- const dotenv = require('dotenv');
- dotenv.config()
- const app = express();
- const fs = require('fs');
- const port = 3000;
+//the env connection should be at the top
+const dotenv = require('dotenv').config();
+
+const express = require('express');
+const  mongoose = require('mongoose');
+
+const Quote = require("./models/Quote");
+
+const PORT = 3000;
+const app = express ();
  
- app.use(express.json());
+// connect to mongo
+mongoose.connect(process.env.MONGO_URL)
+.then (() => console.log('Mongo coneccted'))
+.catch(err => console.log(err));
 
- function getquotes(){
-   const data = fs.readFileSync('quotes.json');
-   return JSON.parse(data);
- }
- const connectDB = async () => {
-   try{
-    const connection = await mongoose.connect(process.env.MONGO_URI)
-    console.log("db connected successfully")
-   }catch(error){
-      console.log("connection error:",error.message)
-      process.exit(1)
-   }
- }
- connectDB()
+//route
+app.get("/", async(req,res) =>{
+    try {
+        const quotes = await Quote.find();
 
- /*function getRandomquote() {
-    const randomIndex = Math.floor(Math.random() * quotes.length);
-    return quotes[randomIndex];
- }*/
- app.get("/",(req,res)=>{
-    const quotes = getquotes();
-    const randomIndex = Math.floor(Math.random()* quotes.length);
-    res.send(`The  today's quote: <br>${quotes[randomIndex].text}`);
- });
- app.listen(port, ()=>{
-    console.log("The server is running");
- })
+        if (quotes.length ===0){
+            return res.send('No quotes found!');
+        }
+
+       const randomIndex = Math.floor(Math.random() * quotes.length);
+       const randomQuote = quotes[randomIndex];
+       
+       res.send (`
+        <h2>Today's quote</h2>
+        <p>"${randomQuote.text}"</p>
+        <p><strong>- ${randomQuote.author}</strong></p>
+        `);
+
+    } catch (error){
+        res.status(500).send("Error fetching quotes");
+    
+    }
+     });
+     //start server
+    app.listen(PORT,() => {
+        console.log(`Server running on port ${PORT}`);
+    });
